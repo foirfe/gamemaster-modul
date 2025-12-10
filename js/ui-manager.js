2491696a507efca65d7a97e89a195f198d316596
livmikala <livgun01@iba.dk>
2025-12-10 22:39:17 +0100

    dropdown til valg mellem vand eller land

--- js/ui-manager.js
+++ js/ui-manager.js
@@ -5,6 +5,22 @@ const sidebar = document.getElementById("task-sidebar");
 //Inputfelt til scenarienavn
 const input = document.createElement("input");
 input.type = "text";
-input.id = "scenario-name";
+input.id = "scenarie-name";
 input.placeholder = "Skriv scenarienavn";
 sidebar.appendChild(input);
+
+//Dropdown til lands/vands
+const select = document.createElement("select");
+select.id = "scenarie-type";
+
+const optionLand = document.createElement("option");
+optionLand = "land";
+optionLand.textContent = "Land";
+select.appendChild(optionLand);
+
+const optionWater = document.createElement("option");
+optionWater = "water";
+optionWater.textContent = "vand";
+select.appendChild("optionWater");
+
+sidebar.appendChild(select);
