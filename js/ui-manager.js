ec1047fadf041c0dcfccf1f774ea9ada808cf5da
livmikala <livgun01@iba.dk>
2025-12-10 22:32:09 +0100

    input-felt tilføjet

--- js/ui-manager.js
+++ js/ui-manager.js
@@ -1,1 +1,10 @@
 //UI MANAGER
+//Fanger task-sidebar
+const sidebar = document.getElementById("task-sidebar");
+
+//Inputfelt til scenarienavn
+const input = document.createElement("input");
+input.type = "text";
+input.id = "scenario-name";
+input.placeholder = "Skriv scenarienavn";
+sidebar.appendChild(input);
